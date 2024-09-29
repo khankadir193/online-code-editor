5f365bb4c54b59c95f789e7388c7ec5ae140e0ce
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-09-29 17:35:43 +0530

    handle resize function has been implemented

--- src/EditorComponents/CodeEditor.js
+++ src/EditorComponents/CodeEditor.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useRef,useEffect } from 'react';
 import MonacoEditor from '@monaco-editor/react';
 import '../App.css';
-const CodeEditor = ({language,value,onChange}) => {
+import debounce from 'lodash/debounce'; // Use lodash debounce or implement a custom debounce function
+
+const CodeEditor = ({ language, value, onChange }) => {
+  const editorRef = useRef(null);
+
+  // Debounced handleResize function to limit the rate of calls to layout()
+  const handleResize = debounce(() => {
+    if (editorRef.current) {
+      editorRef.current.layout();  // Recalculate layout when window resizes
+    }
+  }, 100);  // Adjust the debounce time as needed
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const handleEditorDidMount = (editor) => {
+    editorRef.current = editor;
+  };
   return (
     <MonacoEditor
-     height='40vw'
-     language={language}
-     theme='vs-dark'
-     value={value}
-     onChange={(newValue)=>{onChange(newValue)}}
+      height='40vw'
+      language={language}
+      theme='vs-dark'
+      value={value}
+      onChange={(newValue) => { onChange(newValue) }}
     //  width='85vw'
+      onMount={handleEditorDidMount}
+
     />
   )
 }
